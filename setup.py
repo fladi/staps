a20cb1cced3271d69c2e6994a71cad2166fce903
Michael Fladischer <FladischerMichael@fladi.at>
2016-07-18 20:56:48 +0200

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def read(*names, **kwargs):
 
 setup(
     name='staps',
-    version='0.1.2',
+    version='0.1.3',
     license='BSD',
     description='Simple Throw-Away Publish/Subscribe',
     long_description='%s\n%s' % (
