44fad3b58e258ad6b57c3f0bab0982a0d05bc03b
Michael Fladischer <FladischerMichael@fladi.at>
2016-07-19 19:52:11 +0200

    Improved packaging to include missing templates.

--- setup.py
+++ setup.py
@@ -24,13 +24,9 @@
 
 import io
 import re
-from glob import glob
-from os.path import basename
 from os.path import dirname
 from os.path import join
-from os.path import splitext
 
-from setuptools import find_packages
 from setuptools import setup
 
 
@@ -55,9 +51,9 @@ setup(
     author='Michael Fladischer',
     author_email='michael@openservices.at',
     url='https://github.com/fladi/staps',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    packages=['staps'],
+    package_dir={'staps': 'src/staps'},
+    package_data={'staps': ['*.html']},
     include_package_data=True,
     zip_safe=False,
     classifiers=[
